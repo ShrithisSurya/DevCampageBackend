f5a3332d6e23559fc3d2080402a528f6ab79e823
Shrithissurya <shrithis2002@gmail.com>
2025-01-31 13:07:21 +0530

    Backend Code (User Management) adding one new post request and update the Admin (User details) at 1.07pm

--- models.py
+++ models.py
@@ -44,7 +44,7 @@ class Billing_Address(Document):
     meta={'collection':'billing_address'}
     
     id = StringField(primary_key=True, default=lambda: str(uuid4()))
-    user=ReferenceField(User,null=True)
+    user=ReferenceField(User,reverse_delete_rule=CASCADEs,null=True)
     address_one=StringField(required=True)
     address_two=StringField(required=True)
     city=StringField(required=True)
